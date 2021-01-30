15e209a0f33151ea89a8612c297b0e655702b830
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2021-01-30 23:11:54 +0800

    finished portfolio

---
+++ src/components/about.jsx
@@ -0,0 +1,206 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        {
+          id: "Java",
+          content: "Java",
+          porcentage: "90%",
+          value: "90"
+        },
+        {
+          id: "C-sharp",
+          content: "C#",
+          porcentage: "90%",
+          value: "90"
+        },
+        {
+          id: "Java Springboot",
+          content: "Java Springboot",
+          porcentage: "85%",
+          value: "85"
+        },
+        {
+          id: "Python_skill",
+          content: "Python",
+          porcentage: "85%",
+          value: "85"
+        },
+        {
+          id: "FastAPI",
+          content: "Python FastAPI",
+          porcentage: "85%",
+          value: "85"
+        },
+        {
+          id: "SQL",
+          content: "SQL Relational Database",
+          porcentage: "85%",
+          value: "85"
+        },
+        {
+          id: "NoSQL",
+          content: "NoSQL",
+          porcentage: "85%",
+          value: "85"
+        },
+        {
+          id: "C-sharp",
+          content: ".NET Framework",
+          porcentage: "75%",
+          value: "75"
+        },
+        {
+          id: "JavaScript_skill",
+          content: "JavaScript",
+          porcentage: "70%",
+          value: "70"
+        },
+        {
+          id: "AWS",
+          content: "AWS",
+          porcentage: "70%",
+          value: "70"
+        },
+        {
+          id: "Microsoft Azure",
+          content: "Microsoft Azure",
+          porcentage: "70%",
+          value: "70"
+        },
+        {
+          id: "R",
+          content: "R",
+          porcentage: "70%",
+          value: "70"
+        },
+        {
+          id: "ReactJS",
+          content: "ReactJS",
+          porcentage: "70%",
+          value: "70"
+        },
+        { id: "PHP_skill", content: "PHP Laravel", porcentage: "60%", value: "60" },
+
+        {
+          id: "HTML5",
+          content: "HTML5",
+          porcentage: "60%",
+          value: "60"
+        },
+        {
+          id: "CSS",
+          content: "CSS",
+          porcentage: "60%",
+          value: "60"
+        }
+
+      ],
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            "Hi there, my name is Eugene. I’m an avid learner and technologist, primarily specialising in application development and server side API development. But at the same time, I’m working towards growing myself in the front-end stack, and building myself up as a Full Stack Engineer fluent across every part of the delivery chain."
+        },
+        {
+          id: "second-p-about",
+          content:
+            "My core experience lies in Java, C#, RDBMS, Unix shell scripting, multithreading, deployment on various web hosting services and implementing REST-ful web services. I’ve worked on multiple Java Spring boot projects across my professional experience, and I’ve also worked on the Unity Engine using C# as well."
+        },
+        {
+          id: "third-p-about",
+          content:
+            "In my free time, I’ve built a couple of APIs for my own pet projects. I’ve built these APIs using Python and NoSQL as well, so if you’d like, please check out my work below!"
+        },
+        {
+          id: "fifth-p-about",
+          content:
+            "I’ve also had project experience and day to day involvement in designing system architecture landscapes within the Financial Industry under mentorship of Technology Architecture/Digital Enterprise Architecture Team. Through these experiences, I’ve had the opportunity to define solutions architecture with vendor technology teams as part of the architecture team to fit the bank’s digital enterprise landscape."
+        },
+        {
+          id: "sixth-p-about",
+          content:
+            "I’d like to continue growing in this space of Solutions Architecture and software engineering, so please feel free to reach out below, I’d love to have a chat with you!"
+        },
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="row">
+                      <div
+                        className="col-sm-6 col-md-5"
+                        style={{ margin: "0 auto" }}
+                      >
+                        <div
+                          className="about-img"
+                          style={{ textAlign: "center" }}
+                        >
+                          <img
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="skill-mf">
+                      {/* <p className="title-s">Skill</p> */}
+                      {this.state.skills.map(skill => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <span>{skill.content}</span>{" "}
+                            <span className="pull-right">
+                              {skill.porcentage}
+                            </span>
+                            <div className="progress">
+                              <div
+                                className="progress-bar"
+                                role="progressbar"
+                                style={{ width: skill.porcentage }}
+                                aria-valuenow={skill.value}
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">About Me</h5>
+                      </div>
+                      {this.state.about_me.map(content => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/portfolio.jsx
@@ -0,0 +1,196 @@
+import React from "react";
+
+//import stock
+import learnhub from "../img/LearnhubPoster.png";
+import remgisOverall from "../img/OverallREMGIS.png";
+import remgis1 from "../img/REMGIS1.png";
+import remgis2 from "../img/REMGIS2.png";
+import remgis3 from "../img/REMGIS3.png";
+import remgis4 from "../img/REMGIS4.png";
+import bot from "../img/TranslatorBot.png";
+import CF1 from "../img/CF1.png";
+import CF2 from "../img/CF2.png";
+import CF3 from "../img/CF3.png";
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id="work" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Portfolio</h3>
+                <p className="subtitle-a">
+                  Please feel free to check out some of the stuff I've worked on!
+                </p>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={learnhub} data-lightbox="gallery-vmarine">
+                  <div className="work-img">
+                    <img src={learnhub} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">E-learning portal</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 C# .Net MSSQL Azure
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div className="col-md-4">
+                  <a href="https://wiki.smu.edu.sg/is480/IS480_Team_wiki:_2017T1_PlusMinus">View more</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={remgisOverall} data-lightbox="gallery-aguadeluz">
+                  <div className="work-img">
+                    <img src={remgisOverall} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Geospatial Analytics</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 Javascript R-shiny R <div className="row"><br /></div>
+                          </span>{" "}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={remgis1}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={remgis2}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={remgis3}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={remgis4}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <div className="col-md-4">
+                  <a href="https://wiki.smu.edu.sg/1718t2is415g1/REMGIS_Proposal">View more</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={bot} data-lightbox="gallery-todo">
+                  <div className="work-img">
+                    <img src={bot} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Translator Chatbot</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            Python Heroku <div className="row"><br /></div>
+                          </span>
+                          {/*} / <span className="w-date">18 Sep. 2018</span>*/}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div className="col-md-4">
+                  <a href="https://github.com/eugenetan01/TranslatorBot">View more</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={CF1} data-lightbox="gallery-medlingos">
+                  <div className="work-img">
+                    <img src={CF1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">E-commerce Inventory Backend</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            GoogleAPI REST ReactJS NextJS
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={CF2}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={CF3}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section >
+    );
+  }
+}
+
+
+export default Portfolio;
