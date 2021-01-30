6adfef15c109b75909bba5328f56816e22cd0257
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2021-01-30 23:21:37 +0800

    Updates

---
+++ precache-manifest.6e9b7249a0cfbcdb97b4856ccbab678a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e45420cd3848bd8c0ef58b9d589b381",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3fc9d651598048a31f66",
+    "url": "/portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "04b63aa37508cee30f6b",
+    "url": "/portfolio/static/css/main.61fec683.chunk.css"
+  },
+  {
+    "revision": "3fc9d651598048a31f66",
+    "url": "/portfolio/static/js/2.a34d4ef4.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/portfolio/static/js/2.a34d4ef4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04b63aa37508cee30f6b",
+    "url": "/portfolio/static/js/main.1b31af17.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "a98ae1f5b670a6a672d0d81d5f414901",
+    "url": "/portfolio/static/media/CF1.a98ae1f5.png"
+  },
+  {
+    "revision": "0a5e554c9f8ca2574ab4e8f15946edcd",
+    "url": "/portfolio/static/media/CF2.0a5e554c.png"
+  },
+  {
+    "revision": "9c1fd470c6064a020747854f60edf49b",
+    "url": "/portfolio/static/media/CF3.9c1fd470.png"
+  },
+  {
+    "revision": "3d202b8f451a88cf6025c2af17bd58c2",
+    "url": "/portfolio/static/media/LearnhubPoster.3d202b8f.png"
+  },
+  {
+    "revision": "5eed5906b68392ce581c33f2b2687ac2",
+    "url": "/portfolio/static/media/OverallREMGIS.5eed5906.png"
+  },
+  {
+    "revision": "9a349edd34a3f3974bdf6a2f24bc92a7",
+    "url": "/portfolio/static/media/REMGIS1.9a349edd.png"
+  },
+  {
+    "revision": "7285da762ff7f15a532577d23a0a34eb",
+    "url": "/portfolio/static/media/REMGIS2.7285da76.png"
+  },
+  {
+    "revision": "a528b08a4b39260dbe9981978797b2a7",
+    "url": "/portfolio/static/media/REMGIS3.a528b08a.png"
+  },
+  {
+    "revision": "bdc8f01aac051104044d2a186951f656",
+    "url": "/portfolio/static/media/REMGIS4.bdc8f01a.png"
+  },
+  {
+    "revision": "457d45f27e6c84f14cf18e3d54b14c78",
+    "url": "/portfolio/static/media/TranslatorBot.457d45f2.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/portfolio/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/portfolio/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/portfolio/static/media/myImage.bd80794f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EugenePortfolio/precache-manifest.24817aeeca6a4e73f63ad0be03c45bd6.js"
+  "/portfolio/precache-manifest.6e9b7249a0cfbcdb97b4856ccbab678a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EugenePortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
