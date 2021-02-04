41d6947e70484fda1ca1fdae5b0e5bbc893357af
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2021-02-04 14:16:39 +0800

    removed unused imports

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import imageOverlay from "../img/earth.jpg";
-import { Icon, InlineIcon } from '@iconify/react';
-import vscoIcon from '@iconify-icons/simple-icons/vsco';
-
 
 class Contact extends React.Component {
   render() {
